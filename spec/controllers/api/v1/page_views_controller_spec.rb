906208152b6661952de7b6ff59b527a9784948ef
Adam Rachman <arachman@macbookpro.lan>
2014-07-25 10:49:30 -0700

    spec problem

--- spec/controllers/api/v1/page_views_controller_spec.rb
+++ spec/controllers/api/v1/page_views_controller_spec.rb
@@ -1,5 +1,4 @@
-require 'spec_helper'
-describe Api::V1::PageViewsController do
+describe Api::V1::PageViewsController , :type => :controller do
   before do
     @page_views = [
       PageView.create!(:api_key => 'FILDTEOIK2HBORODV', :ip_address => '192.168.1.1', :page_url => 'www.google.com'),
@@ -7,21 +6,12 @@ describe Api::V1::PageViewsController do
     ]
   end
 
-  context 'foo' do
-    subject do
-      get '/api/v1/page_views?api_key=12345&ip_filters[]=192.158.1.1&url_filters[]=www.google.com'
-      JSON.parse(response.body)
-    end
-
-    it 'should return a list of page views' do
-      should == {
-
-      }
+  describe 'page_views.json' do
+    it "returns a 200 HTTP status" do
+      request.env["HTTP_ACCEPT"] = "application/json"
+      get 'index', { :api_key => "12345" }.to_json
+      expect(response).to be_success
     end
   end
-  #describe "GET #show" do
-  #  before do
-  #    get :show, :api_key 
-  #  end
-  #end
+
 end
